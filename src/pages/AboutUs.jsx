390fc0452d98842b59c36ddaec6c4e770d3cd37f
Abasiediedu <samuelinokon@gmail.com>
2024-09-02 22:08:04 +0200

    Updated Photo Import

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,13 +1,11 @@
+import couplePhoto from "../assets/images/love-story.jpeg";
+
 const AboutUs = () => {
   return (
     <div className="about-us">
       <h1>About Us</h1>
       <p>[Couple&apos;s Story]</p>
-      <img
-        className="couple__img"
-        src="/src/assets/images/love-story.jpeg"
-        alt="Love Story"
-      />
+      <img className="couple__img" src={couplePhoto} alt="Love Story" />
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
+import coupleImage from "../assets/images/couple.jpeg";
+
 const Home = () => {
   return (
     <div className="home">
       <h1>Welcome to Our Wedding</h1>
       <p>We are so excited to share our special day with you!</p>
-      <img
-        className="couple__img"
-        src="src/assets/images/couple.jpeg"
-        alt="Couple"
-      />
+      <img className="couple__img" src={coupleImage} alt="Couple" />
     </div>
   );
 };
